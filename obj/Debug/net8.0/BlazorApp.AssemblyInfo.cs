8d58e1cbc2760392485e1bf8b3b930d0b925db0c
Marie Marcos <mmdevq@gmail.com>
2024-05-28 18:02:29 -0700

    todo items

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18fb261a8364c53397711064056a892b760a029")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd4bc7b26a1e1aa94236e0486f170479c0c7fa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
